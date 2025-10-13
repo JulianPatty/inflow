658e80791786c523f0e25feb12851df713016a71
Java Julian <help@setn.ai>
2025-10-13 15:49:32 -0400

    Integrate TRPC and React Query for data fetching; add client and server setup. Update package dependencies for TRPC and React Query in package.json and package-lock.json. Create new API route for TRPC and implement client-side data fetching component.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,25 @@
-import prisma from "@/lib/db";
+"use client";
+
+import { getQueryClient, trpc } from "@/trpc/server";
+import { Client } from "./client";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Suspense } from "react";
+  
+const Page = async () => {
+  const queryClient = getQueryClient();
+
+  void queryClient.prefetchQuery(trpc.getUsers.queryOptions());
 
 
-const page = async () => {
-  const users = await prisma.user.findMany();
-  return <div>page
-    {users.map((user) => (
-      <div key={user.id}>{user.name}</div>
-    ))}
-
+  return (
+    <div className="min-h-screen min-w-screen flex items-center justify-center">
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <Suspense fallback={<div>Loading...</div>}>
+      <Client />
+      </Suspense>
+      </HydrationBoundary>
     </div>
+  )
 };
 
-export default page;
+export default Page;
